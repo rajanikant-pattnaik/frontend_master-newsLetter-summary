5a5a7a8599519c08891f234acf5ade2369cf9f78
unknown <rjpattnaik12345@gmail.com>
2023-06-20 00:37:56 +0530

    another commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function validateEmail() {
     // Regular expression pattern for email validation
-    const email=document.getElementsByClassName('input');
+    const emailItem=document.getElementById('input');
+    const label=document.getElementById('invalid');
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
+    const email=emailItem.value;
     // Check if the email matches the pattern
+    console.log(email);
     console.log(emailRegex.test(email));
+    document.getElementById('input').value='';
+    emailItem.style.backgroundColor='pink';
+    document.getElementById('invalid').value='invalid email'
     // return emailRegex.test(email);
   }
   
