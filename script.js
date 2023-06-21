4df9574572039f2ea66a6cefa52ff16e61d94463
unknown <rjpattnaik12345@gmail.com>
2023-06-21 21:16:16 +0530

    another commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function validateEmail() {
-    // Regular expression pattern for email validation
     const emailItem=document.getElementById('input');
     const label=document.getElementById('invalid');
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const email=emailItem.value;
-    // Check if the email matches the pattern
-    console.log(email);
-    console.log(emailRegex.test(email));
-    document.getElementById('input').value='';
-    emailItem.style.backgroundColor='pink';
-    document.getElementById('invalid').value='invalid email'
-    // return emailRegex.test(email);
+    const check=emailRegex.test(email);
+    console.log(check)
+    if(check){
+      // document.getElementById('form-part').style.display='none';
+    }
+    else{
+      emailItem.style.backgroundColor='orange'
+    }
   }
   
   
